40fbdd1f080b319272aa7c5dd95e91abd95fa0ea
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-01-28 19:21:44 +0100

    Created components - Sidebar, Header. Adding styled components to wrap content - Main and StyledAppLayout (element layout -> grid). Initial styling (ultimately view for desktop).

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -1,12 +1,34 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const StyledAppLayout = styled.div`
+    display: grid;
+    grid-template-columns: 26rem 1fr;
+    grid-template-rows: auto 1fr;
+    height: 100vh;
+`;
+
+const Main = styled.main`
+    background-color: green;
+    padding: 4rem 4.8rem 6.4rem;
+`;
+
+
 
 function AppLayout() {
-    return (
-        <div>
-            <p>APP LAYOUT</p>
-            <Outlet/>
-        </div>
-    )
+	return (
+		<StyledAppLayout>
+			<Header />
+			<Sidebar />
+			<Main>
+				<Outlet />
+			</Main>
+		</StyledAppLayout>
+	);
 }
 
-export default AppLayout
+export default AppLayout;
+
+// Utworzone komponenty - Sidebar, Header. Dodanie styled conponents do owinięcia treści - Main i StyledAppLayout( układ elementów -> grid ). Wstępne stylowanie (docelowo view dla desktop).
