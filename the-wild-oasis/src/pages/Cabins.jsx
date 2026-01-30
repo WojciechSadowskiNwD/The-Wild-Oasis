70eb34722a020dd1236401055cee65a4f46f29a6
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-01-30 20:13:46 +0100

    All cabin images have been moved to supabese buckets. TanStack Query (React Query v4) has been installed. queryClient has been created in App.jsx and the entire application has been wrapped with the QueryClientProvider component. React Query Dev Tools has been installed.

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -6,12 +6,15 @@ import { getCabins } from "../services/apiCabins";
 function Cabins() {
 	useEffect(function () {
 		getCabins().then((data) => console.log(data));
+     
+
 	}, []);
 
 	return (
 		<Row type="horizontal">
 			<Heading as="h1">All cabins</Heading>
 			<p>TEST</p>
+      <img src="https://nnhgegfzhjdvnchsfzxf.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
 		</Row>
 	);
 }
