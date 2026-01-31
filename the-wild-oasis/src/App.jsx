d0c65149786b51b59ffa1a9e2b08d96f147910f4
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-01-31 18:12:37 +0100

    ReactQueryDevtools added in App.jsx, CabinTable component expanded, useQuery used in it - fetching data from the cabins table. Conditional display of Spinner on isLoading. Table creation in UI. CabinRow component skeleton created - for displaying the contents of cabins in a UI table loop (.map).

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import GlobalStyles from "./styles/GlobalStyles.js";
 
 import Dashboard from "./pages/Dashboard";
@@ -23,6 +25,8 @@ const queryClient = new QueryClient({
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -47,3 +51,5 @@ function App() {
 export default App;
 
 // 
+
+
