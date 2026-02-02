bd10742e66c71060dddcaeb13476c38918466468
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-02-02 17:21:33 +0100

    The react-hook-form library has been installed. The CreateCabinForm form component has been expanded—the useForm hook has been introduced, input fields have been registered, and a basic test function for handling submitted data has been created.

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles.js";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -45,10 +45,10 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-			<Toaster 
+			<Toaster
 				position="top-center"
 				gutter={12}
-				containerStyle={{margin: "8px"}}
+				containerStyle={{ margin: "8px" }}
 				toastOptions={{
 					success: {
 						duration: 3000,
@@ -65,11 +65,11 @@ function App() {
 					},
 				}}
 			/>
-
 		</QueryClientProvider>
 	);
 }
 
 export default App;
 
-//  
+// Zainstalowano
+
