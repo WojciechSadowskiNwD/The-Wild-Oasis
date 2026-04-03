dfa8b15562163e232ed403e3e669cf977e6abf86
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-04-03 11:38:47 +0200

    Using searchParams and the returned props value, the ctive parameter was set for the styling of the selected filter button.

--- the-wild-oasis/src/ui/Filter.jsx
+++ the-wild-oasis/src/ui/Filter.jsx
@@ -37,6 +37,8 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
 	const [searchParams, setSearchParams] = useSearchParams();
+	const currentFilter = searchParams.get(filterField) ||  options.at(0).value;
+
 
 	function handleClick(value) {
 		//te otrzymane do funkcji wartości chcę przekazać do adresu url
@@ -51,6 +53,7 @@ function Filter({ filterField, options }) {
 				<FilterButton
 					key={option.value}
 					onClick={() => handleClick(option.value)}
+					active={option.value === currentFilter}
 				>
 					{option.label}
 				</FilterButton>
