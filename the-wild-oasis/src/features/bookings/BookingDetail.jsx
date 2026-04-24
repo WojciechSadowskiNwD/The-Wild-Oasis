ae0e827f62ef623add8917b3333dcef513ec2c93
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-04-24 14:20:33 +0200

    Create component Booking and extended logic inside CheckinBooking. Add new route fot that.

--- the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -9,6 +9,9 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import Spinner from "../../ui/Spinner";
+import { useNavigate } from "react-router-dom";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -16,11 +19,15 @@ const HeadingGroup = styled.div`
   align-items: center;
 `;
 
-function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
 
+function BookingDetail() {
+  const {booking, isLoading } = useBooking();
   const moveBack = useMoveBack();
+  const navigate = useNavigate();
+
+  if(isLoading) return <Spinner/>
+
+  const {status, id: bookingId } = booking;
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -32,7 +39,7 @@ function BookingDetail() {
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
@@ -41,6 +48,11 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === "unconfirmed" && (
+                    <Button onClick={()=> navigate(`/checkin/${bookingId}`)} >
+                      Check in
+                    </Button>
+                  )}
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
